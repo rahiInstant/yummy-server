bda08f0d630f25b6dda5b703a850466706b0d9c5
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-05-14 04:35:10 +0600

    add set token route

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-require('dotenv').config()
+require("dotenv").config();
 const jsonWebToken = require("jsonwebtoken");
 const app = express();
-const port = process.env.PORT|5000;
+const port = process.env.PORT | 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@foodnest.clyvw7v.mongodb.net/?retryWrites=true&w=majority&appName=foodNest`;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,7 +23,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,17 +32,21 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.post("/jwt", async (req, res) => {
+      console.log("thank you");
+      console.log(req.body);
+      res.send(req.body);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Eat, enjoy testy food.");
