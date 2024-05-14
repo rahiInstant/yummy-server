0307c80d23de6e7934ce6a6922777749cc9fcfc6
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-05-14 06:16:55 +0600

    add food and get food root

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const jsonWebToken = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT | 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@foodnest.clyvw7v.mongodb.net/?retryWrites=true&w=majority&appName=foodNest`;
@@ -32,12 +32,40 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+    const yummyDB = client.db("foodNest");
+    const foodCollection = yummyDB.collection("food");
+
     app.post("/jwt", async (req, res) => {
       console.log("thank you");
-      console.log(req.body);
-      res.send(req.body);
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .send({ success: true });
     });
-
+    app.post("/out", async (req, res) => {
+      console.log(req.body);
+      res
+        .clearCookie("access_token", { httpOnly: true, maxAge: 0 })
+        .send({ logout: true });
+    });
+    app.post("/add-food", async (req, res) => {
+      const foodInfo = req.body;
+      const result = await foodCollection.insertOne(foodInfo);
+      res.send(result);
+    });
+    app.get("/get-food", async (req, res) => {
+      const query = {};
+      const option = {
+        projection: { name: 1, photo: 1, category: 1, price: 1, quantity: 1 },
+      };
+      const result = await foodCollection.find(query, option).toArray();
+      res.send(result);
+    });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
